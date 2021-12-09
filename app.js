700543606d0a4355f32a5f72938013c125874e12
Gbadamosi Odofin <odofing@gmail.com>
2021-12-09 10:16:24 +0100

    updated bodyparser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from 'path'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import connectDB from './config/db.js'
 import userRoute from './Routes/userRoute.js'
 import productRoute from './Routes/productRoute.js'
@@ -12,8 +13,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
-
+app.use(bodyParser.json())
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(
   cors({
